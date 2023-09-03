e794c66b7930e066e70dcc4450413b0d04c40e63
Skg4541 <68281074+Skg4541@users.noreply.github.com>
2023-09-03 12:18:22 +0000

    bug fix

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-let d=true;
-let a=Math.floor(Math.random()*3);
-let b="";
+let d = true;
+let a = Math.floor(Math.random() * 3);
+let b = "";
 
-while(d){
-if(a==0){
-  b="R"
-}
-else if(a==1){
-  b="P"&
-}
-else{
-  b="S"
-}
+while (d) {
+  if (a == 0) {
+    b = "R"
+  }
+  else if (a == 1) {
+    b = "P"
+  }
+  else {
+    b = "S"
+  }
 
-let c=prompt("Choose R for rock , P for paper or S for scissor")
-  alert("Opponent Chose " + (b=="R"?"Rock":(b=="P"?"Paper":"Scissor")));
+  let c = prompt("Choose R for rock , P for paper or S for scissor")
+  alert("Opponent Chose " + (b == "R" ? "Rock" : (b == "P" ? "Paper" : "Scissor")));
 
-if((b=="R"&&c=="P")||(b=="P"&&c=="S")||(b=="S"&&c=="R")){
-  alert("You Won");
-  break;
-}
-else if((c=="R"&&b=="P")||(c=="P"&&b=="S")||(c=="S"&&b=="R")){
-  alert("You Lost")
-  break;
-}
-else{
-  a=Math.floor(Math.random()*3);
-  d= confirm("You Drew. Do you want to Continue ? ")
-}
+  if ((b == "R" && c == "P") || (b == "P" && c == "S") || (b == "S" && c == "R")) {
+    alert("You Won");
+    break;
+  }
+  else if ((c == "R" && b == "P") || (c == "P" && b == "S") || (c == "S" && b == "R")) {
+    alert("You Lost")
+    break;
+  }
+  else {
+    a = Math.floor(Math.random() * 3);
+    d = confirm("You Drew. Do you want to Continue ? ")
+  }
 }
 
